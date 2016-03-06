862b151af33f2e547b88dc1d1a270060eb5ab2b8
gomesuit <gomesuit@gmail.com>
2016-03-06 23:36:59 +0900

    docker-compose

--- provision.sh
+++ provision.sh
@@ -21,3 +21,10 @@ service docker start
 docker run hello-world
 
 yum -y install git
+
+
+curl -L http://github.com/docker/compose/releases/download/1.4.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
+
